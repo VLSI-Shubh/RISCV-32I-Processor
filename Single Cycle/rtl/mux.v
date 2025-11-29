f979d902258de12e828477b371915a6b2ff3f90b
Shubham Upadhyay <vlsi.shubh@gmail.com>
2025-11-29 04:08:06 -0500

    Delete Single Cycle directory

--- Single Cycle/rtl/mux.v
+++
@@ -1,9 +0,0 @@
-module mux #(parameter width = 32)(
-    input [width-1:0] in0, in1,
-    input sel,
-    output reg [width-1:0] out
-);
-    always @(*) begin
-        out = (sel) ? in1 : in0;
-    end
-endmodule
